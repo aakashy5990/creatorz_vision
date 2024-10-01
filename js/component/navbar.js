75cb9bc38acb3812612c7a8d4e734395a91caebe
aakashy5990 <aakashy5990@gmail.com>
2024-10-01 11:02:54 +0530

    Done

--- js/component/navbar.js
+++ js/component/navbar.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the current path of the HTML file
     const currentPath = window.location.pathname;
     
-    // Calculate how deep the current HTML file is in the folder structure
     const depth = currentPath.split('/').length - 2; // "-2" to exclude the domain and file name
     
-    // Build the correct relative path to navbar.html by adding "../" based on the depth
     let navbarPath = "";
     for (let i = 0; i < depth; i++) {
         navbarPath += "../";
     }
-    navbarPath += "component/navbar/navbar.html";  // Add the navbar.html at the end
-
-    // Fetch the navbar.html and insert it into the page
+    navbarPath += "creatorz_vision/component/navbar/navbar.html";
+    
     fetch(navbarPath)
         .then(response => response.text())
         .then(data => {
